4b8c7af7fc0e6d1d6a3097c4cb2baf7b7dec6bf6
ashan-ali <alimohdashan@gmail.com>
2025-10-01 21:34:33 +0530

    diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -22,26 +22,34 @@ st.write("=" * 33)
 st.sidebar.title("Sample Patients Record ") 
 if st.sidebar.button("!!Click!!"):
 	st.sidebar.dataframe(data) 
-g, bp = st.columns(2) 
-g.number_input("Enter Glucose label ==>")
-bp.number_input("Enter BloodPressure ==>")
-sk, isu = st.columns(2) 
-sk.number_input("Enter SkinThickness ==>")
-isu.number_input("Enter Insulin ==>")
-bmi, dp = st.columns(2) 
-bmi.number_input("Enter BMI ==>")
-dp.number_input("Enter DiabetesPedigreeFunction ==>")
-age, name = st.columns(2) 
-age.number_input("Enter age ==>")
-name.text_input("Enter patient name ==>") 
+c1, c2 = st.columns(2) 
+with c1:
+	g = st.number_input("Enter Glucose label ==>")
+with c2:
+	bp=st.number_input("Enter BloodPressure ==>")
+c3, c4 = st.columns(2) 
+with c3:
+	sk=st.number_input("Enter SkinThickness ==>")
+with c4:
+	isu= st.number_input("Enter Insulin ==>")
+c5, c6 = st.columns(2) 
+with c5:
+	bmi= st.number_input("Enter BMI ==>")
+with c6:
+	dp= st.number_input("Enter DiabetesPedigreeFunction ==>")
+c7, c8 = st.columns(2) 
+with c7:
+	age= st.number_input("Enter age ==>")
+with c8:
+	name =st.text_input("Enter patient name ==>") 
 
 res = model.predict([[g, bp, sk, isu, bmi, dp, age]])
 t = res[0]
 
 if st.button("Predict Diabetes "):
 	if t == 1:
-		st.write("You have Diabetes") 
+		st.write(f"{name}!You have Diabetes") 
 	else:
-		st.write("No Diabetes") 
+		st.write(f"{name}!No Diabetes") 
 	
 	
